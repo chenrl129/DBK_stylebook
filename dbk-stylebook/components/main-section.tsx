9d5777fae743d91860930134b3da355d5bd2c570
alston <alston.chan22@sitechhs.com>
2023-10-19 21:09:56 -0400

    Added markdown editor

--- dbk-stylebook/components/main-section.tsx
+++ dbk-stylebook/components/main-section.tsx
@@ -1,6 +1,11 @@
 import { Separator } from "@/components/ui/separator";
 import { supabase } from '@/lib/api';
 import { useEffect, useState } from 'react';
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
+
+import { PenSquare } from "lucide-react"
+import { Button } from "@/components/ui/button"
 
 interface MainSectionProps {
     searchInput: string;
@@ -8,6 +13,8 @@ interface MainSectionProps {
 
 const MainSection: React.FC<MainSectionProps> = ({ searchInput }) => {
     const [data, setData] = useState<any[]>([]);
+    const [editedContent, setEditedContent] = useState<string>('');
+    const [isEditing, setIsEditing] = useState<string | null>(null);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -28,70 +35,88 @@ const MainSection: React.FC<MainSectionProps> = ({ searchInput }) => {
     const lowercasedSearchInput = searchInput.toLowerCase();
 
     const highlightText = (text: string) => {
-        if (searchInput.length < 2) {
-            return text;
-        } else {
-            const parts = text.split(new RegExp(`(${searchInput})`, 'gi'));
-            return <span> { parts.map((part, i) => 
+        const parts = text.split(new RegExp(`(${searchInput})`, 'gi'));
+        return <span>{parts.map((part, i) =>
             part.toLowerCase() === lowercasedSearchInput 
                 ? <span key={i} className="bg-yellow-300">{part}</span> 
                 : part 
-            )} </span>;
-        }
-    }
+        )}</span>;
+    };
 
     const filteredData = data.filter(item => 
         item.term.toLowerCase().includes(lowercasedSearchInput) || 
         item.definition.toLowerCase().includes(lowercasedSearchInput)
     );
 
-    const groupByLetter = (items: any[]) => {
-        return items.reduce((result, item) => {
-            const letter = item.letter;
-            if (!result[letter]) {
-                result[letter] = [];
-            }
-            result[letter].push(item);
-            return result;
-        }, {} as { [key: string]: any[] });
+    const groupedByLetter = filteredData.reduce((groups, item) => {
+        const letter = item.letter;
+        if (!groups[letter]) {
+            groups[letter] = [];
+        }
+        groups[letter].push(item);
+        return groups;
+    }, {} as { [key: string]: any[] });
+
+    const updateContent = async (id: number, content: string) => {
+        const { error } = await supabase
+            .from('stylebook')
+            .update({ definition: content })
+            .eq('id', id);
+
+        if (error) console.error('Error updating content:', error);
+        else alert('Content updated successfully!');
     };
 
-    const groupedData = groupByLetter(filteredData);
+    const sortedLetters = Object.keys(groupedByLetter).sort();
 
     return (
         <div>
-            {Object.keys(groupedData).map((letter, index) => (
+            {sortedLetters.map((letter, index) => (
                 <div key={index} className="mb-4">
                     <div className="mb-8">
                         <h1 className="text-3xl font-bold text-gray-900 sm:text-4xl">{letter}</h1>
                         <Separator className="my-4"/>
                     </div>
-                    {groupedData[letter].map((item: any, itemIndex: number) => (
+                    {groupedByLetter[letter].map((item: any, itemIndex: number) => (
                         <div
                             key={itemIndex}
                             id={item.term}
                             className="relative block overflow-hidden rounded-lg border border-gray-100 my-6 p-4 sm:p-6 lg:p-8"
                         >
                             <span className="absolute inset-x-0 bottom-0 h-2 bg-red-500"></span>
-
                             <div className="sm:flex sm:justify-between sm:gap-4">
-                                <div>
-                                    <h3 className="text-lg font-bold text-gray-900 sm:text-xl">
-                                        {highlightText(item.term)}
-                                    </h3>
-
-                                    {item.important && <p className="mt-1 text-xs font-medium text-red-600">Important</p>}
-                                    {item.sports && <p className="mt-1 text-xs font-medium text-red-600">Sports</p>}
-                                    {item.ap && <p className="mt-1 text-xs font-medium text-red-600">AP Deviation</p>}
+                                <div className="w-full flex flex-row justify-between">
+                                    <div>
+                                        <div className="w-full flex flex-row justify-between items-center">
+                                            <h3 className="text-lg font-bold text-gray-900 sm:text-xl">
+                                                {highlightText(item.term)}
+                                            </h3>
+                                        </div>
+                                        
+                                        {item.important && <p className="mt-1 text-xs font-medium text-red-600">Important</p>}
+                                        {item.sports && <p className="mt-1 text-xs font-medium text-red-600">Sports</p>}
+                                        {item.ap && <p className="mt-1 text-xs font-medium text-red-600">AP Deviation</p>}
+                                    </div>
+                                    <div className="flex flex-col justify-start">
+                                        <div className="">
+                                            {isEditing === item.term ? (
+                                                <Button variant="outline" className="" onClick={() => { updateContent(item.id, editedContent); setIsEditing(null); }}>Save</Button>
+                                            ) : (
+                                                <Button variant="outline" size="icon" className="" onClick={() => { setIsEditing(item.term); setEditedContent(item.definition); }}>
+                                                    <PenSquare className="h-6 w-6"/>
+                                                </Button>
+                                            )}
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
-
                             <div className="mt-4">
-                            {typeof item.definition === 'string' && item.definition.split('\n').map((line: string, lineIndex: number) => (
-                                <p key={lineIndex} className="text-sm text-gray-500">
-                                    {highlightText(line)}
-                                </p>
-                            ))}
+                                {isEditing !== item.term && typeof item.definition === 'string' && item.definition.split('\n').map((line: any, lineIndex: number) => (
+                                    <p key={lineIndex} className="text-sm text-gray-500">
+                                        {highlightText(line)}
+                                    </p>
+                                ))}
+                                {isEditing === item.term && <SimpleMDE value={editedContent} onChange={(value) => setEditedContent(value)} />}
                             </div>
                         </div>
                     ))}
--- dbk-stylebook/lib/import-json.js
+++ dbk-stylebook/lib/import-json.js
@@ -1,12 +1,15 @@
 const fs = require('fs');
 const data = JSON.parse(fs.readFileSync('stylebook.json', 'utf8'));
 
-let csv = 'category,term,important,sports,ap,definition\n';  // header
+let csv = 'id,category,term,important,sports,ap,definition\n';
+let id = 1; 
+
 for (const category in data) {
     data[category].forEach(item => {
         const term = Object.keys(item)[0];
         const attributes = item[term];
-        csv += `${category},"${term}",${attributes.important},${attributes.sports},${attributes.ap},"${attributes.definition.replace(/"/g, '""')}"\n`;
+        csv += `${id},${letter},"${term}",${attributes.important},${attributes.sports},${attributes.ap},"${attributes.definition.replace(/"/g, '""')}"\n`;
+        id++;
     });
 }
 
