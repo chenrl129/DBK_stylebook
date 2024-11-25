67f1c6ccdc060b33f9a606424210980575ca107e
Sameer <sameerc13@outlook.com>
2024-11-25 14:37:53 -0500

    finished import-json & added transform script

--- dbk-stylebook/lib/import-json.js
+++ dbk-stylebook/lib/import-json.js
@@ -1,17 +1,42 @@
 const fs = require('fs');
-const data = JSON.parse(fs.readFileSync('stylebook.json', 'utf8'));
+
+const data = JSON.parse(fs.readFileSync('transformedData.json', 'utf8'));
 
 let csv = 'id,letter,term,important,sports,ap,definition\n';
-let id = 1; 
+let id = 1;
 
-for (const letter in data) {
-    data[letter].forEach(item => {
-        const term = Object.keys(item)[0];
-        const attributes = item[term];
-        csv += `${id},${letter},"${term}",${attributes.important},${attributes.sports},${attributes.ap},"${attributes.definition.replace(/"/g, '""')}"\n`;
-        id++;
-    });
-}
+// creating the csv rows
+data.forEach(item => {
+    csv += `${id},${item.letter},"${item.term}",${item.important},${item.sports},${item.ap},"${item.definition.replace(/"/g, '""')}"\n`;
+    id++;
+});
 
-fs.writeFileSync('data.csv', csv);
-console.log('Data written to data.csv');
+// Writing it to a new file in case we need to hold on to the old data
+fs.writeFileSync('new_data.csv', csv);
+console.log('Data written to new_data.csv');
+
+
+
+
+//This is the orginal script below
+
+
+
+// const fs = require('fs');
+// const data = JSON.parse(fs.readFileSync('stylebook.json', 'utf8'));
+
+// let csv = 'id,letter,term,important,sports,ap,definition\n';
+// let id = 1; 
+
+// for (const letter in data) {
+//     data[letter].forEach(item => {
+//         const term = Object.keys(item)[0];
+//         const attributes = item[term];
+//         csv += `${id},${letter},"${term}",${attributes.important},${attributes.sports},${attributes.ap},"${attributes.definition.replace(/"/g, '""')}"\n`;
+//         id++;
+//     });
+// }
+
+// fs.writeFileSync('data.csv', csv);
+// console.log('Data written to data.csv');
+
---
+++ dbk-stylebook/lib/transform.js
@@ -0,0 +1,38 @@
+//Commenting this out so it doesn't create a new file everytime the site is run
+
+// //This file transforms the original stylebook.json to have a new key value pair, with the key being the first letter of the term.
+
+// const fs = require('fs');
+
+// const data = JSON.parse(fs.readFileSync('stylebook.json', 'utf8'));
+
+// const transformedData = [];
+
+// for (const letter in data) {
+//     data[letter].forEach(item => {
+//         const term = Object.keys(item)[0];
+//         const attributes = item[term];
+//         //determine the letter based on the first letter of the term
+//         const firstLetter = term.charAt(0).toUpperCase();
+//         // Add the term and its attributes to the transformed data
+//         transformedData.push({
+//             letter: firstLetter,
+//             term: term,
+//             important: attributes.important,
+//             sports: attributes.sports,
+//             ap: attributes.ap,
+//             definition: attributes.definition
+//         });
+//     });
+// }
+
+// //Sorting it because certain letters were being mixed because of the original placement
+// transformedData.sort((a, b) => {
+//     if (a.term < b.term) return -1;
+//     if (a.term > b.term) return 1;
+//     return 0;
+// });
+
+// // writing to a new file to preserve the original data
+// fs.writeFileSync('stylebook.json', JSON.stringify(transformedData, null, 2));
+// console.log('Transformed data written to transformedData.json');
